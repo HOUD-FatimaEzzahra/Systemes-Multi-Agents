449e49329bc76e3308f246406bb6b0d20314c84b
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-05-05 00:11:07 +0100

    fixing errors

--- TP_SM1_NEW/src/main/java/ma/enset/tp_sm1_new/MainContainer.java
+++ TP_SM1_NEW/src/main/java/ma/enset/tp_sm1_new/MainContainer.java
@@ -6,12 +6,6 @@ import jade.wrapper.AgentContainer;
 import jade.wrapper.ControllerException;
 
 public class MainContainer {
-    // hadchi mzyan ikhrj haka?
-    //ah rah aslan kaan kharj flwl
-    //kaykhes ikhrj w mli n creyi agent w n executiii l'interface graphique ykhdm kolchi
-    // interface graphiue dial server ola client ?
-    // bzouj
-    //ma kaynch aslan hnaya server
     public static void main(String[] args) throws ControllerException {
         Runtime runtime=Runtime.instance();
         ProfileImpl profile=new ProfileImpl();
